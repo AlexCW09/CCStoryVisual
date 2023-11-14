a0d5a6e3e525e18df75490b71cb7d8e3309e9028
Jonathan Loving <eprisencc@gmail.com>
2023-11-14 10:34:29 -0500

    Page turn effect.

--- SwiftUIStoryTemplate/Chapter2View.swift
+++ SwiftUIStoryTemplate/Chapter2View.swift
@@ -1,8 +1,17 @@
 import SwiftUI
+import Pages
 
 struct Chapter2View: View {
+    @State var index: Int = 0
+    
     var body: some View {
-        Text("Chapter 2")
+        Pages(currentPage: $index, navigationOrientation: .horizontal,
+              transitionStyle: .pageCurl) {
+            Text("Welcome! This is Page 1")
+            Text("This is Page 2")
+            Text("...and this is Page 3")
+            Circle() // The 4th page is a Circle
+        }
     }
 }
 
