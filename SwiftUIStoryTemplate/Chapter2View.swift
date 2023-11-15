577d6e0a2fb8d42c63c1a92dc15c594137ffd78f
Jonathan Loving <eprisencc@gmail.com>
2023-11-15 10:39:23 -0500

    Testing text formatting options.

--- SwiftUIStoryTemplate/Chapter2View.swift
+++ SwiftUIStoryTemplate/Chapter2View.swift
@@ -12,7 +12,19 @@ struct Chapter2View: View {
             Text("...and this is Page 3")
             Circle() // The 4th page is a Circle
         }*/
-        Text("Hello World!")
+        VStack {
+            Spacer()
+            Text("Chapter 2")
+                .font(.largeTitle)
+                .bold()
+            ScrollView {
+                Text("It was a cold morning and years had passed since youth. Age had only added to their insatiable curiosity and love for adventure. Every day was a new quest, an opportunity to solve the mysteries of the world. The townsfolk relied on them, beckoning them to undertake daring feats, from vanquishing menacing creatures to reuniting lost souls and restoring crumbling edifices. But one fateful day, a hushed whisper traveled through the village, carried on the wings of the wind, reaching 's ears. It was a request unlike any other.\n")
+                    .padding(25)
+                JustifiedText("This is some really, really, really long text so showcase how show justified text in SwiftUI, spanning over multiple lines. Unfortunately, this doesn't work with regular Text view, nor does it work with AttributedString.")
+                    .padding(25)
+                
+            }
+        }
     }
 }
 
--- SwiftUIStoryTemplate/ContentView.swift
+++ SwiftUIStoryTemplate/ContentView.swift
@@ -29,6 +29,27 @@ struct ContentView: View {
     }
 }
 
+struct JustifiedText: UIViewRepresentable {
+  private let text: String
+  private let font: UIFont
+
+  init(_ text: String, font: UIFont = .systemFont(ofSize: 18)) {
+    self.text = text
+    self.font = font
+  }
+
+  func makeUIView(context: Context) -> UITextView {
+    let textView = UITextView()
+    textView.font = font
+    textView.textAlignment = .justified
+    return textView
+  }
+
+  func updateUIView(_ uiView: UITextView, context: Context) {
+    uiView.text = text
+  }
+}
+
 #Preview {
     ContentView()
 }
